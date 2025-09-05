85a4a6f85f8da744fc327818fe4ad1e6f84a19cd
isachiketsaw <sachi629955@gmail.com>
2025-09-05 23:14:54 +0530

    new update

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+const Main = () => {
+  return (
+    <div className="main">
+        <div className="nav">
+            <p>Gemini</p>
+            <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello,Monkey</span></p>
+                <p>how can i help you</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+                 <div className="card">
+                    <p>Brifly sumarize this concept: uarban planning</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                 <div className="card">
+                    <p>Brainstorm team bonding activities for our work retreat</p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+                 <div className="card">
+                    <p>IMprove the readablity of the following code</p>
+                    <img src={assets.code_icon} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
