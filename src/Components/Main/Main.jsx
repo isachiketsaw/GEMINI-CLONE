3bf27c12c7b828dd831f454952b925ba7d0e793e
isachiketsaw <sachi629955@gmail.com>
2025-09-06 21:21:52 +0530

    This is towadsy update

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,39 +1,56 @@
-import React from 'react'
-import './Main.css'
-import { assets } from '../../assets/assets'
+import React from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
 const Main = () => {
   return (
     <div className="main">
-        <div className="nav">
-            <p>Gemini</p>
-            <img src={assets.user_icon} alt="" />
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello,Monkey</span>
+          </p>
+          <p>how can i help you</p>
         </div>
-        <div className="main-container">
-            <div className="greet">
-                <p><span>Hello,Monkey</span></p>
-                <p>how can i help you</p>
-            </div>
-            <div className="cards">
-                <div className="card">
-                    <p>suggest beautiful places to see on an upcoming road trip</p>
-                    <img src={assets.bulb_icon} alt="" />
-                </div>
-                 <div className="card">
-                    <p>Brifly sumarize this concept: uarban planning</p>
-                    <img src={assets.compass_icon} alt="" />
-                </div>
-                 <div className="card">
-                    <p>Brainstorm team bonding activities for our work retreat</p>
-                    <img src={assets.message_icon} alt="" />
-                </div>
-                 <div className="card">
-                    <p>IMprove the readablity of the following code</p>
-                    <img src={assets.code_icon} alt="" />
-                </div>
-            </div>
+        <div className="cards">
+          <div className="card">
+            <p>suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brifly sumarize this concept: uarban planning</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>IMprove the readablity of the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
         </div>
-    </div>
-  )
-}
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a prompt here" />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
 
-export default Main
+          <p className="bottom-info">
+            Gemini may display inacurate info ,including about people so
+            double-check its response.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
